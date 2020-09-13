dfcd5a14c4ca86c67ddafb17a92980cbb2633eee
Ogre Yoga <ogreyoga@192-168-1-17.tpgi.com.au>
2020-09-13 19:49:12 +1000

    implmenet multi-upload and base64 image conversion through multer

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -3,6 +3,7 @@ import path from 'path'
 import multer from 'multer'
 
 const { Entry, Topic } = require('../database/models/')
+const fs = require('fs')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -47,11 +48,63 @@ async function singleUpload (req, res) {
 }
 
 async function multiUpload (req, res) {
+  // 10 is the limit I've defined for number of uploaded files at once
+  // 'multiple_images' is the name of our file input field
+  const upload = multer({ storage: storage, fileFilter: imageFilter }).array('group', 10)
 
+  upload(req, res, function (err) {
+    if (req.fileValidationError) {
+      return res.status(400).json({ message: `${req.fileValidationError}` })
+    } else if (!req.files) {
+      return res.status(400).json({ message: 'Please select file(s) for upload.' })
+    } else if (err instanceof multer.MulterError) {
+      return res.status(500).json({ message: err })
+    } else if (err) {
+      return res.status(500).json({ message: err })
+    }
+
+    let result = 'You have uploaded these images: <ul>'
+    const files = req.files
+    let index, len
+
+    // Loop through all the uploaded images and display them on frontend
+    for (index = 0, len = files.length; index < len; ++index) {
+      result += `<li>${files[index].path}</li>`
+    }
+    result += '</ul>'
+    res.status(200).json({ message: `You have uploaded these files: ${result}.` })
+  })
 }
 
 async function dbUpload (req, res) {
+  try {
+    const upload = multer({ storage: storage, fileFilter: imageFilter }).single('icon')
 
+    upload(req, res, function (err) {
+      if (req.fileValidationError) {
+        return res.status(400).json({ message: `${req.fileValidationError}` })
+      } else if (!req.file) {
+        return res.status(400).json({ message: 'Please select file for upload.' })
+      } else if (err instanceof multer.MulterError) {
+        return res.status(500).json({ message: err })
+      } else if (err) {
+        return res.status(500).json({ message: err })
+      }
+
+      var img = fs.readFileSync(req.file.path)
+      var encode_image = img.toString('base64')
+      // Define a JSONobject for the image attributes for saving to database
+
+      var finalImg = {
+        contentType: req.file.mimetype,
+        image: new Buffer.from(encode_image, 'base64')
+      }
+
+      res.status(200).json({ message: `You have converted this file to base64, length: ${finalImg.image.length}.` })
+    })
+  } catch (error) {
+    res.status(500).json({ message: `File could not be uploaded: ${error.message}` })
+  }
 }
 
 export { singleUpload, multiUpload, dbUpload }
